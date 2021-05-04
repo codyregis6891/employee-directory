576d1a003c2f40f54261e01a33e5d334d043eebe
Cody Regis <codyregis6891@yahoo.com>
2021-05-03 20:13:00 -0700

    file structure done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; import
+  'bootstrap-css-only/css/bootstrap.min.css'; import
+  'mdbreact/dist/css/mdb.css';
+import Jumbotron from '../src/components/Jumbotron/Jumbotron';
+import Results from "../src/components/Results.js/Results"
+import Search from './components/Search/Search';
+
+function App() {
+  return (
+    <div>
+      <Jumbotron />
+      <Results />
+    </div>
+
+  );
+}
+
+export default App;
